e531bc66c01db2eabff9776ef51cf7893934ab05
sky <helloheroemail@gmail.com>
2022-11-19 21:12:17 +0800

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <>
+      <div className="h-1/2 w-full flex md:flex-row flex-col p-20 justify-center text-center   ">
+        <ul>
+          <p className="text-black font-bold text-3xl pb-6 dark:text-white">
+            Net Shinobi
+          </p>
+
+          <p className="text-black font-bold text-3xl pb-6 dark:text-white">
+            Copyright © 2022 Net Shinobi
+          </p>
+        </ul>
+      </div>
+    </>
+  );
+}
